7fab82bd1f31fde13f7b6ee8608eb6423d743295
Morgan Jones <integ3rs@gmail.com>
2012-06-23 14:19:36 -0600

    * Add: !argnews * Join both #valvearg2 and #valvearg3 for Rake tests

--- lib/argbot/cmd/wiki.rb
+++ lib/argbot/cmd/wiki.rb
@@ -12,7 +12,13 @@ module ARGBot
       focus = @@wiki.get_page('Current_Focus')
       s.reply "Current focus: #{focus.strip}" unless focus.nil?
     end
+
+    def wk_news(s, args)
+      news = @@wiki.get_page('Template:News')
+      s.reply "Latest news: #{news.match(/^\s*?\*(?<news>.+)\s*$/)[1].gsub(/'+/, '')}" unless news.nil?
+    end
   end
   
   cmd :wiki, :wk_status, [:status, :s], 'Displays ARG status'
+  cmd :wiki, :wk_news, [:news, :n], 'Displays the latest ARG news entry.'
 end
